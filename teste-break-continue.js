5ea38f0ad498d971e1c7304880c6b0c6ee710540
renato aires <rentonaires@gmail.com>
2020-08-12 14:26:43 -0300

    commit de ajustes

--- teste-break-continue.js
+++ teste-break-continue.js
@@ -1,10 +1,12 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+/**
+ * break so tem resultados no for e no switch
+ */
 for( let x in nums){
     if(x == 5){
-        break
+        break //saida do loop 
     } 
-    console.log('x = ' + nums[x]);
+    console.log(`x = ${ nums[x] }`);
 }
 
 for(let y in nums){
